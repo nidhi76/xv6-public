0efb1191756a6f17e020272e7bbf505b27451244
nidhi76 <nidhi.wagh7@gmail.com>
2021-05-07 19:09:13 +0530

    endsem

--- syscall.c
+++ syscall.c
@@ -103,6 +103,8 @@ extern int sys_unlink(void);
 extern int sys_wait(void);
 extern int sys_write(void);
 extern int sys_uptime(void);
+extern int sys_getstats(void);
+extern int countcalls;
 
 static int (*syscalls[])(void) = {
 [SYS_fork]    sys_fork,
@@ -126,11 +128,13 @@ static int (*syscalls[])(void) = {
 [SYS_link]    sys_link,
 [SYS_mkdir]   sys_mkdir,
 [SYS_close]   sys_close,
+[SYS_getstats]   sys_getstats,
 };
 
 void
 syscall(void)
-{
+{ 
+  countcalls++;
   int num;
   struct proc *curproc = myproc();
 
